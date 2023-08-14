5b66184f59410dbda6e456b3dd46e42756ce3af6
cakeisdead <1744036+cakeisdead@users.noreply.github.com>
2023-08-13 20:18:40 -0600

    Reading into an array and counting items from the search list

--- src/main.rs
+++ src/main.rs
@@ -4,12 +4,14 @@ use xmltree::Element;
 
 fn main() {
     let food_search_list = vec!["Colombian Waffles", "Fire Toast"];
+    let mut data: Vec<String> = vec![];
+
     println!("## Searched Items:");
-    for item in food_search_list {
+    for item in &food_search_list {
         println!("{}", item);
     }
 
-    println!("## Files:");
+    println!("\n## Files:");
     let files = fs::read_dir("./files").unwrap();
     
     for file in files {
@@ -17,11 +19,17 @@ fn main() {
         let file_name = f.unwrap().file_name();
         let xml_path = f.unwrap().path();
         println!("File: {:?}", file_name);
-        read_xml(&xml_path).unwrap();
+        read_xml(&xml_path, &mut data).unwrap();
+    }
+
+    println!("\n## Search results:");
+    for item in &food_search_list {
+        let qty = data.iter().filter(|&n| *n == item.to_string()).count();
+        println!("{0} x{1}", item, qty);
     }
 }
 
-fn read_xml(xml_path: &PathBuf) -> io::Result<()> {
+fn read_xml(xml_path: &PathBuf, data: &mut Vec<String>) -> io::Result<()> {
     let mut f = File::open(xml_path)?;
     let mut buffer = String::new();
 
@@ -33,7 +41,9 @@ fn read_xml(xml_path: &PathBuf) -> io::Result<()> {
         let sub_child = child.as_element().unwrap().get_child("name");
         for sub_sub in &sub_child.unwrap().children 
         {
-            println!("\t{}", sub_sub.as_text().unwrap());
+            let food = sub_sub.as_text().unwrap();
+            data.push(food.to_string());
+            println!("\t Added {0} into summary vector.", food);
         }
     }
     Ok(())
